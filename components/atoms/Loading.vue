726e7458318d89b1e8e402c6494be1772e0dc47a
seita_matsumoto <gyabooo@gmail.com>
2020-02-09 19:41:28 +0900

    update

--- components/atoms/Loading.vue
+++ components/atoms/Loading.vue
@@ -1,39 +1,70 @@
-<template lang='pug'>
+<template lang="pug">
   div.a-loading
     font-awesome-icon(:icon="['fas', 'spinner']").a-loading__icon.a-blinking
 </template>
 
 <script>
+export default {}
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .a-loading {
   text-align: center;
 }
 .a-blinking {
-	-webkit-animation:blink 1.5s ease-in-out infinite alternate;
-    -moz-animation:blink 1.5s ease-in-out infinite alternate;
-    animation:blink 1.5s ease-in-out infinite alternate;
+  -webkit-animation: blink 1.5s ease-in-out infinite alternate;
+  -moz-animation: blink 1.5s ease-in-out infinite alternate;
+  animation: blink 1.5s ease-in-out infinite alternate;
 }
 @-webkit-keyframes blink {
-    0% {opacity:0;}
-    25% {opacity:1;}
-    50% {opacity:0;}
-    75% {opacity:1;}
-    100% {opacity:0;}
+  0% {
+    opacity: 0;
+  }
+  25% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  75% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
 }
 @-moz-keyframes blink {
-    0% {opacity:0;}
-    25% {opacity:1;}
-    50% {opacity:0;}
-    75% {opacity:1;}
-    100% {opacity:0;}
+  0% {
+    opacity: 0;
+  }
+  25% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  75% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
 }
 @keyframes blink {
-    0% {opacity:0;}
-    25% {opacity:1;}
-    50% {opacity:0;}
-    75% {opacity:1;}
-    100% {opacity:0;}
+  0% {
+    opacity: 0;
+  }
+  25% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  75% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
 }
 </style>
