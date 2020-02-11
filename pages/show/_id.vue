e8d5cd9dbc5299e32c74448d66bf00b1b5115c3d
seita_matsumoto <gyabooo@gmail.com>
2020-02-11 16:04:43 +0900

    update

--- pages/show/_id.vue
+++ pages/show/_id.vue
@@ -3,7 +3,7 @@
     SearchBook(:url="url")
     a-layout-header.c-heading.p-show__header
       h1.p-show__title 本の詳細
-    
+    p {{ book }}
 </template>
 
 <script>
@@ -15,7 +15,11 @@ export default {
   },
   data() {
     return {
-      url: '/.netlify/functions/search?title='
+      url: '/.netlify/functions/search?title=',
+      author: this.book.elements.filter(function(el) {
+        if (el.name === 'author') return true
+      })[0].elements[0].text,
+      book: this.$store.state.books.list[this.$route.params.id]
     }
   }
 }
